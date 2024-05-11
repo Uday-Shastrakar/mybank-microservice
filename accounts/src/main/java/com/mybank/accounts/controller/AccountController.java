801a2b6e2a69e6eae33847ac2ab9c24908e7f6a0
Uday Shastrakar <uday.shastrakar@gmail.com>
2024-05-11 21:52:00 +0530

    Mbk-02  added api response documentation

--- accounts/src/main/java/com/mybank/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/mybank/accounts/controller/AccountController.java
@@ -2,9 +2,12 @@ package com.mybank.accounts.controller;
 
 import com.mybank.accounts.constants.AccountConstants;
 import com.mybank.accounts.dto.CustomerDto;
+import com.mybank.accounts.dto.ErrorResponseDto;
 import com.mybank.accounts.dto.ResponseDto;
 import com.mybank.accounts.service.IAccountService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -66,7 +69,10 @@ public class AccountController {
             ),
             @ApiResponse(
                     responseCode = "500",
-                    description = "Http status Internal Server Error"
+                    description = "Http status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
             )
     }
     )
@@ -95,7 +101,10 @@ public class AccountController {
             ),
             @ApiResponse(
                     responseCode = "500",
-                    description = "Http status Internal Server Error"
+                    description = "Http status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
             )
     }
     )
